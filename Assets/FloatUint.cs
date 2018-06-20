51ae60c160846f1d6e0b65d274e24acda7187a3a
高超.Chaos <gaochao.chaos@yanfa.com>
2018-06-20 13:31:33 +0800

    float2uint

---
+++ Assets/FloatUint.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System.Runtime.InteropServices;
+
+namespace Chaos {
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct FloatUint
+    {
+        [FieldOffset(0)]
+        uint _uint;
+        [FieldOffset(0)]
+        float _float;
+
+        public static implicit operator FloatUint(float f)
+        {
+            FloatUint fu = new FloatUint() {_float = f};
+            return fu;
+        }
+
+        public static implicit operator FloatUint(uint u)
+        {
+            FloatUint fu = new FloatUint() {_uint = u};
+            return fu;
+        }
+
+        public static implicit operator float(FloatUint fu)
+        {
+            return fu._float;
+        }
+
+        public static implicit operator uint(FloatUint fu)
+        {
+            return fu._uint;
+        }
+    }
+}
---
+++ Assets/Half.cs
@@ -0,0 +1,21 @@
+﻿using Chaos;
+
+public struct Half
+{
+    ushort _value;
+
+    public static explicit operator Half(float f)
+    {
+        uint u = (FloatUint)f;
+        Half ret = new Half() {_value = (ushort)(((u >> 16) & 0x8000) | ((((u & 0x7f800000) - 0x38000000) >> 13) & 0x7c00) | ((u >> 13) & 0x03ff))};
+        return ret;
+    }
+
+    public static implicit operator float(Half h)
+    {
+        ushort s = h._value;
+        uint u = (uint)(((s & 0x8000) << 16) | (((s & 0x7c00) + 0x1C000) << 13) | ((s & 0x03FF) << 13));
+        float ret = (FloatUint)u;
+        return ret;
+    }
+}
